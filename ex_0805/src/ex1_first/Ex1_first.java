83417f40c63eb18f28a3e6680e36a1aee073ecec
kds116400-stack <kds116400@gmail.com>
2026-08-06 14:13:17 +0900

    0805 수업내용

---
+++ ex_0805/src/ex1_first/Ex1_first.java
@@ -0,0 +1,16 @@
+package ex1_first;
+
+public class Ex1_first {
+    //main 메서드는 프로그램이 실행될때 가장 먼저 호출되는 시작점
+    public static void main(String[] args) {
+       //주석 : 컴파일시에 jvm이 인식하지 못하는
+       //개발자들간의 소통을 위한 코드
+            System.out.println(1+1);
+            System.out.println("1+1="+(1+1));
+            System.out.println(1+1+"안녕"+1+1);
+   
+            
+            
+                    
+   }
+}
