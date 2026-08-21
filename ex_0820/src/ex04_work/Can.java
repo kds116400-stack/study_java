3444c80246cb411b8d759d7391a55c06cfebb9be
kds116400-stack <kds116400@gmail.com>
2026-08-21 16:08:06 +0900

    0820 수업내용

--- ex_0820/src/ex04_work/Can.java
+++ ex_0820/src/ex04_work/Can.java
@@ -4,18 +4,20 @@ public class Can {
     
     private String name;
     private int price;
+    
+    public Can( String name, int price ){
+        this.name = name;
+        this.price = price;
+    }
+
     public String getName() {
         return name;
     }
-    public void setName(String name) {
-        this.name = name;
-    }
+   
     public int getPrice() {
         return price;
     }
-    public void setPrice(int price) {
-        this.price = price;
-    }
+   
 
     
 
--- ex_0820/src/ex07_work/Graph.java
+++ ex_0820/src/ex07_work/Graph.java
@@ -1,17 +1,31 @@
 package ex07_work;
 
-public class Graph {
-    
-    public static void main(String[] args) {
-        
-        //0~ 9사이의 난수를 100개 발생시키고
-        //각 숫자가 몇개씩 만들어졌는지를 그래프화 하시오
-        //------------------
-        //0의 갯수 : ############ 12
-        //1의 갯수 : ####### 7
-        //   ...
-        //9의 갯수 : ########## 10
+import java.util.Random;
 
-    }//main
+public class Graph {
+
+    public static void main(String[] args) {
+
+        // 0~ 9사이의 난수를 100개 발생시키고
+        // 각 숫자가 몇개씩 만들어졌는지를 그래프화 하시오
+        // ------------------
+        // 0의 갯수 : ############ 12
+        // 1의 갯수 : ####### 7
+        // ...
+        // 9의 갯수 : ########## 10
+
+        int[] arr = new int[10];
+
+        Random rnd = new Random();
+
+        for (int i = 0; i < 100; i++) {
+            int r = rnd.nextInt(10);
+            arr[r]++;
+        } // for
+
+        PrintGraph pg = new PrintGraph();
+        pg.print(arr);
+
+    }// main
 
 }
