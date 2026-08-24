4c907d0759bb60dbc2d20be43ed9b9bc9e00be3b
kds116400-stack <kds116400@gmail.com>
2026-08-24 16:02:40 +0900

    0821 수업내용

--- ex_0821/src/ex11_work/WorkMain.java
+++ ex_0821/src/ex11_work/WorkMain.java
@@ -1,5 +1,7 @@
 package ex11_work;
 
+import java.util.Scanner;
+
 public class WorkMain {
     
     public static void main(String[] args) {
@@ -9,7 +11,33 @@ public class WorkMain {
         //ealpp 오답
         //>> apple
         //apple정답 !!
-        String[] strArr = {"APPLE", "ORANGE", "HOPE", "VIEW"};
+        WorkSub ws = new WorkSub();
+        String answer = ws.getAnswer();
+
+        //정답을 썩어서 문제로 만들기
+        String question = ws.scrambleWord(answer);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("문제 : " + question );
+
+        while(true){
+
+            System.out.print(">> ");
+            String str = sc.next();
+
+            if( str.equalsIgnoreCase(answer) ){
+                System.out.println(str + "은 정답입니다");
+                break;
+            }else{
+                System.out.println(str + "은 정답이 아닙니다");
+            }
+
+
+
+        }//while
+
+
 
         
     }//main
