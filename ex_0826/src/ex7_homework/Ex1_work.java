cfbd728659fac6b4bc676fbbb7c368bb4287ff2b
kds116400-stack <kds116400@gmail.com>
2026-08-27 16:15:22 +0900

    0826 수업내용

--- ex_0826/src/ex7_homework/Ex1_work.java
+++ ex_0826/src/ex7_homework/Ex1_work.java
@@ -1,14 +1,43 @@
 package ex7_homework;
 
+import java.beans.IntrospectionException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Ex1_work {
     public static void main(String[] args) {
-        
-        //ArrayList를 두 개 만들고 1 ~ 5사이의 난수를 5개 추가해둔다.
-        //두 개 리스트의 값을 비교하여 교집합을 출력
-        //-------------------
-        //[5, 3, 5, 1, 5]
-        //[2, 3, 1, 1, 3]
-        //교집합 : [3, 1]
 
-    }//main
+        // ArrayList를 두 개 만들고 1 ~ 5사이의 난수를 5개 추가해둔다.
+        // 두 개 리스트의 값을 비교하여 교집합을 출력
+        // -------------------
+        // [5, 3, 5, 1, 5]
+        // [2, 3, 1, 1, 3]
+        // 교집합 : [3, 1]
+        Random rnd = new Random();
+        List<Integer> list1 = new ArrayList<Integer>();
+        List<Integer> list2 = new ArrayList<Integer>();
+
+        // 교집합을 담기위한 리스트
+        List<Integer> common = new ArrayList<Integer>();
+
+        for (int i = 0; i < 5; i++) {
+            list1.add(rnd.nextInt(5) + 1);
+            list2.add(rnd.nextInt(5) + 1);
+        } // for
+
+        System.out.println(list1);
+        System.out.println(list2);
+
+        for (int num : list1) {
+
+            if (list2.contains(num) && !common.contains(num)) {
+                common.add(num);
+            }
+
+        } // for
+
+        System.out.println("교집합 : " + common);
+
+    }// main
 }
--- ex_0826/src/ex8_homework/WorkMain.java
+++ ex_0826/src/ex8_homework/WorkMain.java
@@ -1,5 +1,9 @@
 package ex8_homework;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class WorkMain {
     public static void main(String[] args) {
         /*
@@ -19,6 +23,36 @@ public class WorkMain {
          */
         
         String[] data = {"apple", "banana", "grape", "strawberry", "pineapple"};
+        List<String> arr = new ArrayList<String>();
+
+        WorkSub ws = new WorkSub(arr, data);
+        ws.setDaemon(true);
+        ws.start();
+
+       Scanner sc = new Scanner(System.in);
+
+        while (true) {
+
+            if( arr.size() == 0 ){
+                System.out.println("클리어!!!");
+                break;
+            }
+
+            System.out.println(arr);
+            System.out.println(">>");
+            String in = sc.next();
+
+            //사용자가 입력한 단어와 arr에 담긴 값을 비교
+            for( int i = 0; i < arr.size(); i++ ){
+
+                if(in.equals(arr.get(i))){
+                    arr.remove(i);
+                    break;
+                }
+
+            }//for
+            
+        }//while
 
     }//main
 }
